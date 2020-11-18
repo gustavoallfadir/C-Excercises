a8e8341f7b7201cef0d9b3ace0b0e74441d35e35
Gustavo Allfadir <68663539+gustavoallfadir@users.noreply.github.com>
2020-11-17 23:10:50 -0600

    Add files via upload

---
+++ Hello_world/main.cpp
@@ -0,0 +1,10 @@
+//Programa básico: "Hello World"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    printf("Bueno, pues ya está... programa finalizado.\n");
+    return 0;
+}
+
